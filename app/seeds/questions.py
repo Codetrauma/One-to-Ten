ef9d4c385cda19d5db6483ff9ab73b723e63ec4b
Tanner Shaw <traumaaa@gmail.com>
2022-02-04 12:44:32 -0800

    Add Survey Responses seed

--- app/seeds/questions.py
+++ app/seeds/questions.py
@@ -5,7 +5,10 @@ def seed_questions():
   demo_question = Questions(weight=3, text='How much do you like this survey?', one_label='Not at all', ten_label='Extremely', survey_id=1)
   demo_question_2 = Questions(weight=3, text='How much do you like this next survey?', one_label='Not at all', ten_label='Extremely', survey_id=2)
 
-
+  db.session.add(demo_question)
+  db.session.add(demo_question_2)
+  db.session.commit()
+  
 def undo_questions():
   db.session.execute('TRUNCATE questions RESTART IDENTITY CASCADE;')
   db.session.commit()
---
+++ app/seeds/survey_responses.py
@@ -0,0 +1,16 @@
+from app.models import db, SurveyResponses
+
+
+def seed_survey_responses():
+  demo_survey_response = SurveyResponses(user_id=1, survey_id=1)
+  new_survey_response_1 = SurveyResponses(user_id=1, survey_id=2)
+  new_survey_response_2 = SurveyResponses(user_id=2, survey_id=2)
+
+  db.session.add(demo_survey_response)
+  db.session.add(new_survey_response_1)
+  db.session.add(new_survey_response_2)
+  db.session.commit()
+
+def undo_survey_responses():
+  db.session.execute('TRUNCATE survey_responses RESTART IDENTITY CASCADE;')
+  db.session.commit()
