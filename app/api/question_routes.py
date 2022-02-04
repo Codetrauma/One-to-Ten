bd8af54000d5996416fa1d3d0166c0b893971248
Tanner Shaw <traumaaa@gmail.com>
2022-02-04 15:52:38 -0800

    Finished question API routes

--- app/api/question_routes.py
+++ app/api/question_routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from app.models import Questions
+from flask import Blueprint
+from app.models import db, Questions
 
 
 question_routes = Blueprint('questions', __name__)
@@ -14,8 +14,25 @@ def question(id):
     question = Questions.query.get(id)
     return question.to_dict()
 
-
 @question_routes.route('/<int:id>/stats')
 def question_stats(id):
     question = Questions.query.get(id)
+    return question.question_stats.to_dict()
+
+@question_routes.route('/<int:id>/users')
+def question_users(id):
+    question = Questions.query.get(id)
+    return {'users': [user.to_dict() for user in question.question_responses]}
+
+@question_routes.route('/<int:id>/users/<int:user_id>')
+def question_user(id, user_id):
+    question = Questions.query.get(id)
+    return {'user': [user.to_dict() for user in question.question_responses if user.user_id == user_id]}
+
+@question_routes.route('/<int:id>/stats', methods=['UPDATE'])
+def question_stats_update(id):
+    question = Questions.query.get(id)
+    question.question_stats.response_count += 1
+    question.question_stats.average = question.question_stats.response_count / question.question_stats.response_count
+    db.session.commit()
     return question.question_stats.to_dict()
