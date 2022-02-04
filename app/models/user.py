2a73d896e509d5e7da0a6b38997a03e7d4c5d68f
Tanner Shaw <traumaaa@gmail.com>
2022-02-04 09:39:13 -0800

    created model files

--- app/models/user.py
+++ app/models/user.py
@@ -7,9 +7,22 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    first_name = db.Column(db.String(40), nullable=False, unique=True)
+    last_name = db.Column(db.String, nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
+    gender = db.Column(db.String, nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    state_abbreviation = db.Column(db.String, nullable=False)
+    biography = db.Column(db.String, nullable=False)
+    facebook = db.Column(db.String)
+    instagram = db.Column(db.String)
+    snapchat = db.Column(db.String)
+    tiktok = db.Column(db.String)
+    twitter = db.Column(db.String)
+    github = db.Column(db.String)
+
 
     @property
     def password(self):
@@ -28,3 +41,12 @@ class User(db.Model, UserMixin):
             'username': self.username,
             'email': self.email
         }
+
+
+    user_locations = db.relationship('UserLocations', back_populates='locations', uselist=False, cascade='all, delete-orphan')
+    user_profile = db.relationship('Profiles', back_populates='profile', uselist=False, cascade='all, delete-orphan')
+    user_preferences = db.relationship('Preferences', back_populates='preferences', uselist=False, cascade='all, delete-orphan')
+    # user_1_matches = db.relationship('Matches', back_populates='user1', cascade='all, delete-orphan')
+    # user_2_matches = db.relationship('Matches', back_populates='user2', cascade='all, delete-orphan')
+    user_question_responses = db.relationship('QuestionResponses', back_populates='u_q_responses', cascade='all, delete-orphan')
+    user_survey_responses = db.relationship('SurveyResponses', back_populates='u_s_responses', cascade='all, delete-orphan')
