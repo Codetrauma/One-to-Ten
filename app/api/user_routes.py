200468615b018a69f5c5f5dc50b6732df7c65fca
Christy Chen <christy.w.chen@gmail.com>
2022-02-06 15:13:55 -0800

    renamed sign up form to user form because form will be used for updating user data. updated email validation in user form so that validation errors for an email exists error is not raised when an existing user is keeping their email the same.

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,7 @@
+from calendar import c
 from flask import Blueprint, jsonify, request
 from flask_login import current_user, login_required
-from app.forms import SignUpForm
+from app.forms import UserForm
 from app.models import db, User
 
 user_routes = Blueprint('users', __name__)
@@ -43,32 +44,32 @@ def user_update(id):
     Update the session user's record if id of record being updated matches current user's id.
     Returns unauthorized error ids do not match.
     """
-    form = SignUpForm()
+    form = UserForm()
     form['csrf_token'].data = request.cookies['csrf_token']
 
     if id == current_user.id:
-        if form.validate_on_submit():
-            user = User.query.get(id)
+        user = User.query.get(id)
 
-            user.first_name = form.data['first_name'],
-            user.last_name = form.data['last_name'],
-            user.email = form.data['email'],
-            user.password = form.data['password'],
-            user.gender = form.data['gender'],
-            user.dob = form.data['dob'],
-            user.city = form.data['city'],
-            user.state_abbreviation = form.data['state_abbreviation'],
-            user.biography = form.data['biography'],
-            user.facebook = form.data['facebook'],
-            user.instagram = form.data['instagram'],
-            user.twitter = form.data['twitter'],
-            user.snapchat = form.data['snapchat'],
-            user.tiktok = form.data['tiktok'],
+        if form.validate_on_submit():
+            user.first_name = form.data['first_name']
+            user.last_name = form.data['last_name']
+            user.email = form.data['email']
+            user.password = form.data['password']
+            user.gender = form.data['gender']
+            user.dob = form.data['dob']
+            user.city = form.data['city']
+            user.state_abbreviation = form.data['state_abbreviation']
+            user.biography = form.data['biography']
+            user.facebook = form.data['facebook']
+            user.instagram = form.data['instagram']
+            user.twitter = form.data['twitter']
+            user.snapchat = form.data['snapchat']
+            user.tiktok = form.data['tiktok']
             user.github = form.data['github']
 
             db.session.commit()
 
-            return user.to_dict()
+            return user.to_dict(current_user)
         else:
             return {'errors': validation_errors_to_error_messages(form.errors)}, 401
     return {'errors': ['Unauthorized']}, 401
--- app/forms/user_form.py
+++ app/forms/user_form.py
@@ -1,15 +1,25 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, DateField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
 
 def user_exists(form, field):
-    # Checking if user exists
+    # Checking if email already exists in the database
+
     email = field.data
     user = User.query.filter(User.email == email).first()
+
+    # If there is a current session user, only raise a user exists error if they are
+    # trying to change their user email.
+    if current_user.is_authenticated:
+        if current_user.to_dict(current_user)['email'] == email:
+            return
+
     if user:
-        raise ValidationError('Email address is already in use.')
+            raise ValidationError('Email address is already in use.')
+
 
 
 # def username_exists(form, field):
@@ -20,7 +30,7 @@ def user_exists(form, field):
 #         raise ValidationError('Username is already in use.')
 
 
-class SignUpForm(FlaskForm):
+class UserForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired(), user_exists])
