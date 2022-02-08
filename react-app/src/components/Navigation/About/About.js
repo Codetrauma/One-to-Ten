f767e8c0356569d1bd70370e6380a1f1fed8d0f1
Tanner Shaw <traumaaa@gmail.com>
2022-02-08 14:30:39 -0800

    About us editing

--- react-app/src/components/Navigation/About/About.js
+++ react-app/src/components/Navigation/About/About.js
@@ -1,31 +1,64 @@
 import './About.css';
-
+import ArrowButton from '../../Forms/ArrowButton/ArrowButton';
 
 const About = () => {
 
   return (
     <div>
-      <h1>The Creators</h1>
+      <h1>The Creators of One-to-Ten</h1>
       <div>
-      <h3> Cameron Whiteside </h3>
-      <a href='https://github.com/CameronWhiteside' target="_blank">Github</a>
-      <a href='https://www.linkedin.com/in/cameronwhiteside/' target="_blank">LinkedIn</a>
+        <h3> Cameron Whiteside </h3>
       </div>
-    <div>
-      <h3> Christy Chen </h3>
-      <a href='https://github.com/christywchen' target="_blank">Github</a>
-      <a href='https://www.linkedin.com/in/christy-chen/' target="_blank">LinkedIn</a>
-    </div>
-    <div>
-      <h3>Aletheia Kim</h3>
-      <a href='https://github.com/akim38' target="_blank">Github</a>
-      <a href=''>LinkedIn</a>
-    </div>
-    <div>
-      <h3>Tanner Shaw</h3>
-      <a href='https://github.com/JTannerShaw/' target="_blank">Github</a>
-      <a href='https://www.linkedin.com/in/tanner-shaw-a25702162/' target="_blank">LinkedIn</a>
-    </div>
+      <div>
+        <ArrowButton>
+          <a href='https://github.com/CameronWhiteside' target="_blank">Github</a>
+        </ArrowButton>
+        <div>
+          <ArrowButton>
+            <a href='https://www.linkedin.com/in/cameronwhiteside/' target="_blank">LinkedIn</a>
+          </ArrowButton>
+        </div>
+      </div>
+      <div>
+        <h3> Christy Chen </h3>
+      </div>
+      <div>
+        <ArrowButton>
+          <a href='https://github.com/christywchen' target="_blank">Github</a>
+        </ArrowButton>
+        <div>
+          <ArrowButton>
+            <a href='https://www.linkedin.com/in/christy-chen/' target="_blank">LinkedIn</a>
+          </ArrowButton>
+        </div>
+      </div>
+      <div>
+        <h3>Aletheia Kim</h3>
+        <div>
+          <ArrowButton>
+            <a href='https://github.com/akim38' target="_blank">Github</a>
+          </ArrowButton>
+        </div>
+        <div>
+          <ArrowButton>
+            <a href='https://www.linkedin.com/in/aletheia-kim-47086922a/'>LinkedIn</a>
+          </ArrowButton>
+
+        </div>
+      </div>
+      <div>
+        <h3>Tanner Shaw</h3>
+        <div>
+          <ArrowButton>
+            <a href='https://github.com/JTannerShaw/' target="_blank">Github</a>
+          </ArrowButton>
+        </div>
+        <div>
+          <ArrowButton>
+            <a href='https://www.linkedin.com/in/tanner-shaw-a25702162/' target="_blank">LinkedIn</a>
+          </ArrowButton>
+        </div>
+      </div>
     </div>
   )
 }
