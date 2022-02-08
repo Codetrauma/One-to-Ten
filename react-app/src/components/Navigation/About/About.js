125c9d6012a0bb04348687fb1752efa3f8629cb1
Tanner Shaw <traumaaa@gmail.com>
2022-02-08 14:40:17 -0800

    I'm terrible at CSS

--- react-app/src/components/Navigation/About/About.js
+++ react-app/src/components/Navigation/About/About.js
@@ -4,35 +4,38 @@ import ArrowButton from '../../Forms/ArrowButton/ArrowButton';
 const About = () => {
 
   return (
-    <div>
+    <div className='about-us-main'>
+      <div className='about-us-header'>
       <h1>The Creators of One-to-Ten</h1>
-      <div>
+      </div>
+      <div className='cameron'>
         <h3> Cameron Whiteside </h3>
-      </div>
-      <div>
-        <ArrowButton>
-          <a href='https://github.com/CameronWhiteside' target="_blank">Github</a>
-        </ArrowButton>
         <div>
           <ArrowButton>
-            <a href='https://www.linkedin.com/in/cameronwhiteside/' target="_blank">LinkedIn</a>
+            <a href='https://github.com/CameronWhiteside' target="_blank">Github</a>
           </ArrowButton>
+          <div>
+            <ArrowButton>
+              <a href='https://www.linkedin.com/in/cameronwhiteside/' target="_blank">LinkedIn</a>
+            </ArrowButton>
+          </div>
         </div>
       </div>
-      <div>
+
+      <div className='christy'>
         <h3> Christy Chen </h3>
-      </div>
-      <div>
-        <ArrowButton>
-          <a href='https://github.com/christywchen' target="_blank">Github</a>
-        </ArrowButton>
         <div>
           <ArrowButton>
-            <a href='https://www.linkedin.com/in/christy-chen/' target="_blank">LinkedIn</a>
+            <a href='https://github.com/christywchen' target="_blank">Github</a>
           </ArrowButton>
+          <div>
+            <ArrowButton>
+              <a href='https://www.linkedin.com/in/christy-chen/' target="_blank">LinkedIn</a>
+            </ArrowButton>
+          </div>
         </div>
       </div>
-      <div>
+      <div className='aletheia'>
         <h3>Aletheia Kim</h3>
         <div>
           <ArrowButton>
@@ -43,10 +46,9 @@ const About = () => {
           <ArrowButton>
             <a href='https://www.linkedin.com/in/aletheia-kim-47086922a/'>LinkedIn</a>
           </ArrowButton>
-
         </div>
       </div>
-      <div>
+      <div className='tanner'>
         <h3>Tanner Shaw</h3>
         <div>
           <ArrowButton>
