a4b9a975f96c9221c73828aa8c020c445a062b18
Tanner Shaw <traumaaa@gmail.com>
2022-02-08 14:16:59 -0800

    About us filled in

--- react-app/src/components/Navigation/About/About.js
+++ react-app/src/components/Navigation/About/About.js
@@ -5,9 +5,27 @@ const About = () => {
 
   return (
     <div>
+      <h1>The Creators</h1>
+      <div>
       <h3> Cameron Whiteside </h3>
-      <a href='https://github.com/CameronWhiteside'>Github</a>
-      <a href=''>Linkedin</a>
+      <a href='https://github.com/CameronWhiteside' target="_blank">Github</a>
+      <a href='https://www.linkedin.com/in/cameronwhiteside/' target="_blank">LinkedIn</a>
+      </div>
+    <div>
+      <h3> Christy Chen </h3>
+      <a href='https://github.com/christywchen' target="_blank">Github</a>
+      <a href='https://www.linkedin.com/in/christy-chen/' target="_blank">LinkedIn</a>
+    </div>
+    <div>
+      <h3>Aletheia Kim</h3>
+      <a href='https://github.com/akim38' target="_blank">Github</a>
+      <a href=''>LinkedIn</a>
+    </div>
+    <div>
+      <h3>Tanner Shaw</h3>
+      <a href='https://github.com/JTannerShaw/' target="_blank">Github</a>
+      <a href='https://www.linkedin.com/in/tanner-shaw-a25702162/' target="_blank">LinkedIn</a>
+    </div>
     </div>
   )
 }
