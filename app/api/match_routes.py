9087b0523b83b1bd673d34e013164876586addab
Christy Chen <christy.w.chen@gmail.com>
2022-02-08 14:08:54 -0800

    added match deletion api route.

--- app/api/match_routes.py
+++ app/api/match_routes.py
@@ -18,3 +18,14 @@ def user_matches(user_id):
 def two_matches(user_id_1, user_id_2):
     matches = Matches.query.filter(Matches.user_1_id == user_id_1, Matches.user_2_id == user_id_2).all()
     return {'user_matches': [match.to_dict() for match in matches]}
+
+@match_routes.route('/<int:user_id_1>/<int:user_id_2>', methods=['DELETE'])
+def delete_match(user_id_1, user_id_2):
+    match1 = Matches.query.filter(Matches.user_1_id == user_id_1, Matches.user_2_id == user_id_2).all()
+    match2 = Matches.query.filter(Matches.user_2_id == user_id_1, Matches.user_1_id == user_id_2).all()
+
+    db.session.delete(match1[0])
+    db.session.delete(match2[0])
+    db.session.commit()
+
+    return {'message': "Match deleted successfully"}
