5d1765205ac4b791c55337f9a5aff28685df1cdd
Christy Chen <christy.w.chen@gmail.com>
2022-02-11 09:30:00 -0800

    added some additional styling to about page, fixed missing dashes in navbar title, added hover link styling to navbar  description

--- react-app/src/components/Navigation/About/About.js
+++ react-app/src/components/Navigation/About/About.js
@@ -4,64 +4,73 @@ import ArrowButton from '../../Forms/ArrowButton/ArrowButton';
 const About = () => {
 
   return (
-    <div className='about-us-main'>
+    <div id="about__container">
       <div className='about-us-header'>
-      <h1>The Creators of One-to-Ten</h1>
+        <h1>One-to-Ten</h1>
+        <p className="p-1 main-color">
+          This project seeks to match like-minded individuals through collaborative surveys whose questions are answered on a scale of 1 to 10. Our algorithm calculates average response of each question, then determines how individual users scale against that average. From there, we match users with who share the highest compatibility.
+        </p>
+        <p className="p-1 main-color">
+          Join us and find your perfect match.
+        </p>
       </div>
-      <div className='cameron'>
-        <h3> Cameron Whiteside </h3>
-        <div>
-          <ArrowButton>
-            <a href='https://github.com/CameronWhiteside' target="_blank">Github</a>
-          </ArrowButton>
+      <h3>The Creators of One-to-Ten</h3>
+      <div className='about-us-main'>
+        <div className='cameron'>
+          <h4>Cameron Whiteside</h4>
           <div>
             <ArrowButton>
-              <a href='https://www.linkedin.com/in/cameronwhiteside/' target="_blank">LinkedIn</a>
+              <a href='https://github.com/CameronWhiteside' className="about__link" target="_blank">Github</a>
             </ArrowButton>
+            <div>
+              <ArrowButton>
+                <a href='https://www.linkedin.com/in/cameronwhiteside/' className="about__link" target="_blank">LinkedIn</a>
+              </ArrowButton>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className='christy'>
-        <h3> Christy Chen </h3>
-        <div>
-          <ArrowButton>
-            <a href='https://github.com/christywchen' target="_blank">Github</a>
-          </ArrowButton>
+        <div className='christy'>
+          <h4> Christy Chen </h4>
           <div>
             <ArrowButton>
-              <a href='https://www.linkedin.com/in/christy-chen/' target="_blank">LinkedIn</a>
+              <a href='https://github.com/christywchen' className="about__link" target="_blank">Github</a>
+            </ArrowButton>
+            <div>
+              <ArrowButton>
+                <a href='https://www.linkedin.com/in/christy-chen/' className="about__link" target="_blank">LinkedIn</a>
+              </ArrowButton>
+            </div>
+          </div>
+        </div>
+        <div className='aletheia'>
+          <h4>Aletheia Kim</h4>
+          <div>
+            <ArrowButton>
+              <a href='https://github.com/akim38' className="about__link" target="_blank">Github</a>
+            </ArrowButton>
+          </div>
+          <div>
+            <ArrowButton>
+              <a href='https://www.linkedin.com/in/aletheia-kim-47086922a/' className="about__link" target="_blank">LinkedIn</a>
+            </ArrowButton>
+          </div>
+        </div>
+        <div className='tanner'>
+          <h4>Tanner Shaw</h4>
+          <div>
+            <ArrowButton>
+              <a href='https://github.com/JTannerShaw/' className="about__link" target="_blank">Github</a>
+            </ArrowButton>
+          </div>
+          <div>
+            <ArrowButton>
+              <a href='https://www.linkedin.com/in/tanner-shaw-a25702162/' className="about__link" target="_blank">LinkedIn</a>
             </ArrowButton>
           </div>
         </div>
       </div>
-      <div className='aletheia'>
-        <h3>Aletheia Kim</h3>
-        <div>
-          <ArrowButton>
-            <a href='https://github.com/akim38' target="_blank">Github</a>
-          </ArrowButton>
-        </div>
-        <div>
-          <ArrowButton>
-            <a href='https://www.linkedin.com/in/aletheia-kim-47086922a/'>LinkedIn</a>
-          </ArrowButton>
-        </div>
-      </div>
-      <div className='tanner'>
-        <h3>Tanner Shaw</h3>
-        <div>
-          <ArrowButton>
-            <a href='https://github.com/JTannerShaw/' target="_blank">Github</a>
-          </ArrowButton>
-        </div>
-        <div>
-          <ArrowButton>
-            <a href='https://www.linkedin.com/in/tanner-shaw-a25702162/' target="_blank">LinkedIn</a>
-          </ArrowButton>
-        </div>
-      </div>
-    </div>
+    </div >
   )
 }
 
--- react-app/src/components/Navigation/SessionNav/SessionNav.js
+++ react-app/src/components/Navigation/SessionNav/SessionNav.js
@@ -82,7 +82,8 @@ function SessionNav({ sessionUser }) {
             <>
                 <div className="nav__bar--child nav__bar--2">
                     <p className="nav__bar--description p-2">
-                        A social experiment designed to connect like-minded individuals with one another. <Link to="/about" className="nav__bar--link underline-slide">Learn More.</Link>
+                        A social experiment designed to connect like-minded individuals with one another. <Link to="/about"
+                            className="nav__link">Learn More.</Link>
                     </p>
                 </div>
                 <div className="nav__bar--child nav__bar--3">
