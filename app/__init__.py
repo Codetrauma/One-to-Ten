693a4ab5d784c6d8caa001b1c87f9087ce0fda3d
Christy Chen <christy.w.chen@gmail.com>
2022-03-11 15:21:44 -0800

    updated path for meta image.

--- app/__init__.py
+++ app/__init__.py
@@ -75,4 +75,6 @@ def inject_csrf_token(response):
 def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
+    if path == 'one-to-ten.jpg':
+        return app.send_static_file('one-to-ten.jpg')
     return app.send_static_file('index.html')
